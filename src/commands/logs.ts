687df6f82f43b3f47c1ef4726696e47bfcaa2393
Mateo Murphy <mateo.murphy@gmail.com>
2019-04-28 14:36:27 -0400

    Implement log tailing

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -4,6 +4,8 @@ import { formatEvent, handleTime } from "../utils";
 
 AWS.config.update({ region: "us-east-1" });
 
+const cloudwatchlogs = new AWS.CloudWatchLogs();
+
 export class LogsCommand extends Command {
   static description = `Retrieves logs`;
 
@@ -23,13 +25,13 @@ export class LogsCommand extends Command {
       char: "e",
       description: "end of the time range",
     }),
+    tail: flags.boolean({ char: "t", description: "tail logs" }),
   };
+
+  lastSeenTime: number | undefined;
 
   async run() {
     const { args, flags } = this.parse(LogsCommand);
-
-    const cloudwatchlogs = new AWS.CloudWatchLogs();
-
     const logGroupName = args.logGroupName;
     const startTime = handleTime(flags.start);
     const endTime = handleTime(flags.end);
@@ -42,12 +44,33 @@ export class LogsCommand extends Command {
       limit: flags.num || 1000,
     };
 
+    this.fetch(params);
+
+    if (flags.tail) {
+      delete params.endTime;
+      setInterval(() => {
+        if (this.lastSeenTime) {
+          params.startTime = this.lastSeenTime;
+        }
+        this.fetch(params);
+      }, 1000);
+    }
+  }
+
+  updateLastSeenTime(time: number | undefined) {
+    this.lastSeenTime = time;
+  }
+
+  fetch(params: AWS.CloudWatchLogs.FilterLogEventsRequest) {
     cloudwatchlogs.filterLogEvents(params, (err, data) => {
       if (err) {
         this.error(err);
       } else {
         if (data.events) {
           data.events.forEach((event) => {
+            if (event.timestamp) {
+              this.updateLastSeenTime(event.timestamp + 1);
+            }
             this.log(formatEvent(event));
           });
         }
