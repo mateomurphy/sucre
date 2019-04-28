48c1162e73e9c0243b16b57913607346b1749691
Mateo Murphy <mateo.murphy@gmail.com>
2019-04-28 10:06:29 -0400

    Convert project to typescript

---
+++ types/parse-duration.d.ts
@@ -0,0 +1,28 @@
+export = index;
+declare function index(str: any): number;
+declare namespace index {
+  const d: number;
+  const day: number;
+  const h: number;
+  const hour: number;
+  const hr: number;
+  const m: number;
+  const microsecond: number;
+  const millisecond: number;
+  const min: number;
+  const minute: number;
+  const month: number;
+  const ms: number;
+  const nanosecond: number;
+  const ns: number;
+  const s: number;
+  const sec: number;
+  const second: number;
+  const w: number;
+  const week: number;
+  const wk: number;
+  const y: number;
+  const year: number;
+  const yr: number;
+  const μs: number;
+}
