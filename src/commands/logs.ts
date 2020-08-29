ef04583f19a820efd635c8691b6b577217eceeeb
Mateo Murphy <mateo.murphy@gmail.com>
2020-08-29 11:34:57 -0400

    Clean up

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -1,32 +1,32 @@
-import {Command, flags} from '@oclif/command'
-import AWS from 'aws-sdk'
-import {handleTime} from '../utils'
+import { Command, flags } from "@oclif/command";
+import AWS from "aws-sdk";
+import { formatEvent, handleTime } from "../utils";
 
-AWS.config.update({region: 'us-east-1'})
+AWS.config.update({ region: "us-east-1" });
 
 export class LogsCommand extends Command {
-  static description = `Retrieves logs`
+  static description = `Retrieves logs`;
 
-  static args = [
-    {name: 'logGroupName'},
-  ]
+  static args = [{ name: "logGroupName" }];
 
   static flags = {
-    env: flags.string({char: 'e', description: 'environment'}),
-    num: flags.integer({char: 'n', description: 'number of lines to display'}),
-    start: flags.string({description: 'start of the time range'}),
-    stop: flags.string({description: 'end of the time range'}),
-  }
+    env: flags.string({ char: "e", description: "environment" }),
+    num: flags.integer({
+      char: "n",
+      description: "number of lines to display",
+    }),
+    start: flags.string({ description: "start of the time range" }),
+    stop: flags.string({ description: "end of the time range" }),
+  };
 
   async run() {
-    const {args, flags} = this.parse(LogsCommand)
+    const { args, flags } = this.parse(LogsCommand);
 
-    const cloudwatchlogs = new AWS.CloudWatchLogs()
+    const cloudwatchlogs = new AWS.CloudWatchLogs();
 
-    const logGroupName = args.logGroupName
-
-    let startTime = handleTime(flags.start)
-    let endTime = handleTime(flags.stop)
+    const logGroupName = args.logGroupName;
+    const startTime = handleTime(flags.start);
+    const endTime = handleTime(flags.stop);
 
     var params = {
       interleaved: true,
@@ -34,18 +34,18 @@ export class LogsCommand extends Command {
       startTime: startTime,
       endTime: endTime,
       limit: flags.num || 1000,
-    }
+    };
 
     cloudwatchlogs.filterLogEvents(params, (err, data) => {
       if (err) {
-        this.error(err)
+        this.error(err);
       } else {
         if (data.events) {
-          data.events.forEach(event => {
-            this.log(event.message)
-          })
+          data.events.forEach((event) => {
+            this.log(formatEvent(event));
+          });
         }
       }
-    })
+    });
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,27 @@
-import parseDuration from 'parse-duration'
-import { isValid, parse } from 'date-fns'
-import { FilteredLogEvent } from 'aws-sdk/clients/cloudwatchlogs'
+import parseDuration from "parse-duration";
+import { isValid, parse } from "date-fns";
+import { FilteredLogEvent } from "aws-sdk/clients/cloudwatchlogs";
 
 export function handleTime(string: string | undefined) {
-  const now = new Date().getTime()
+  const now = new Date().getTime();
 
   if (!string) {
-    return now
+    return undefined;
   }
 
-  let time = parse(string, "yyyy-MM-DD", new Date())
+  const time = parse(string, "yyyy-MM-DD", new Date());
 
   if (isValid(time)) {
-    return time.getTime()
+    return time.getTime();
   }
 
-  return now - Math.abs(parseDuration(string))
+  return now - Math.abs(parseDuration(string));
 }
 
 export function formatEvent(event: FilteredLogEvent) {
-  const date = event.timestamp ? new Date(event.timestamp).toISOString() : 'unknown'
+  const date = event.timestamp
+    ? new Date(event.timestamp).toISOString()
+    : "unknown";
 
-  return `[${date}] (${event.logStreamName}) ${event.message}`
+  return `[${date}] (${event.logStreamName}) ${event.message}`;
 }
